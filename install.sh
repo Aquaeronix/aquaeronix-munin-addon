4fd0bd2c77222753ccfb942280e79bf342e0d530
Dennis Chen <barracks510@gmail.com>
2015-03-26 01:32:05 -0700

    begin install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#install.sh
+#This file is part of Aquaeronix
+#
+#Copyright (C) 2015 barracks510 <barracks510@gmail.com>
+#
+#Aquaeronix is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#Aquaeronix is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#determine OS
+
+#search for munin package install
+
+#install munin if not already installed
+
+#copy plugin files into proper directories
+
+#confirm installation
