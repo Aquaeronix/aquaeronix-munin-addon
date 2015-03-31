46022b8141f68e5bfc0a11d7b4e37043a2a8f338
Dennis Chen <barracks510@gmail.com>
2015-03-30 23:41:23 -0700

    continue install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,28 @@
 #along with Aquaeronix. If not, see <http://www.gnu.org/licenses/>.
 #
 
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+
 #determine OS
+case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
+    Debian)     type="debian" ;;
+    Ubuntu)     type="ubuntu" ;;
+    *)          type="rhel" ;;
+esac
 
 #search for munin package install
+if [ $type == debian || $type == ubuntu ]; then
+    echo 'This system runs Debian or Ubuntu'
+    if [ $(dpkg-query -W -f='${Status}' munin 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        echo 'Munin is not installed.'
+        echo 'Munin will be installed.'
+        apt-get install munin -y
+    fi
+fi
 
 #install munin if not already installed
 
